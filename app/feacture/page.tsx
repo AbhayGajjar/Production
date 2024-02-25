c18d3e83ce10a78bc41e004bc18bd90bddf1e98e
AbhayGajjar <abhaymgajjar@gmail.com>
2024-02-25 13:34:49 +0530

    .

--- app/feacture/page.tsx
+++ app/feacture/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { MdPeopleAlt } from "react-icons/md";
 import { SiHomeassistantcommunitystore } from "react-icons/si";
 
-export const metadata={
-  title:"Freture : NeytRAM"
+export const metadata = {
+  title: "Freture : NeytRAM"
 }
 
 const Feacture = () => {
@@ -17,155 +17,149 @@ const Feacture = () => {
 
       <div className=' mt-3 feacture w-full grid grid-cols-4 gap-2 h-screen px-20'>
 
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <MdPeopleAlt/>
-              </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <MdPeopleAlt />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
 
-            </div>         
-         </div>
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <SiHomeassistantcommunitystore/>
-              </div>
+
+
+
+          </div>
+        </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <SiHomeassistantcommunitystore />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Optical, Exclusively Yours</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'>Optical, Exclusively Yours</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Experience the refined sophistication of Optical Elegance, exclusively crafted to elevate your visual experience with style and function.</p>
 
-            </div>         
-         </div>
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <MdPeopleAlt/>
-              </div>
+
+
+
+          </div>
+        </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <MdPeopleAlt />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'> All-In-One Optics Solution</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Revolutionary optics integrate multiple technologies for peak performance in one user-friendly platform, maximizing efficiency and ease of use</p>
 
-            </div>         
-         </div>
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <MdPeopleAlt/>
-              </div>
+
+
+
+          </div>
+        </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <MdPeopleAlt />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'>Cost Effective</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Budget-friendly solution maximizes efficiency, minimizes expenses while maintaining quality, perfect for cost-conscious consumers seeking high value for money.</p>
 
-            </div>         
-         </div>
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <MdPeopleAlt/>
-              </div>
+
+
+
+          </div>
+        </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <MdPeopleAlt />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'> Mobile Assistance Available</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Free phone support provided as a complimentary service, offering assistance and guidance to customers at no extra cost, ensuring a seamless experience.</p>
 
-            </div>         
-         </div>
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <MdPeopleAlt/>
-              </div>
+
+
+
+          </div>
+        </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <MdPeopleAlt />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'>Frequent Upgrades Included</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Regular updates and improvements are part of the package, ensuring ongoing enhancements to the product or service, keeping it up-to-date and efficient.</p>
 
-            </div>         
-         </div>
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <MdPeopleAlt/>
-              </div>
+
+
+
+          </div>
+        </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <MdPeopleAlt />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'>Unlimited Cloud Storage</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Access to limitless online storage space, allowing users to store and manage files, data, and media without worrying about capacity restrictions.</p>
 
-            </div>         
-         </div>
-         <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
-            <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
-              <div className="image text-6xl ml-2 text-orange-500">
-                <MdPeopleAlt/>
-              </div>
+
+
+
+          </div>
+        </div>
+        <div className=' w-[40vh] h-[20vw] rounded-lg hover: transform transition duration-300 hover:scale-105 hover:shadow-lg hover:shadow-[#16182d]'>
+          <div className="heading-box mt-8 flex justify-center items-center  h-[7vh] ">
+            <div className="image text-6xl ml-2 text-orange-500">
+              <MdPeopleAlt />
             </div>
-            <div className='textatre flex-row items-center mt-2'>
-              <h1 className='text-center text-xl font-medium'>Limitless Coustomers</h1>
-              <div className='h-[10px]'></div>
-              
-                <p className='text-center'>Seamlessly create and oversee your customers in our optical solution, with zero additional fees for customer creation</p>
+          </div>
+          <div className='textatre flex-row items-center mt-2'>
+            <h1 className='text-center text-xl font-medium'>Advanced Prescription Solution</h1>
+            <div className='h-[10px]'></div>
 
-              
-              
+            <p className='text-center'>Cutting-edge software streamlines prescription management, enhancing accuracy and efficiency for optimal patient care and workflow.</p>
 
-            </div>         
-         </div>
 
-       
-        
-       
-         
 
+
+          </div>
+        </div>
       </div>
-      <div className="button flex w-full -mt-5 ">
-             <button className='m-auto px-14 py-3 bg-orange-600 rounded-lg text-white text-[20px] font-medium'>Expolore More</button>
-         </div>
-     
-     
-     
+      <div className="button items-center justify-center flex w-full -mt-5 ">
+        <button className=' px-14 py-3 border-none bg-orange-600 rounded-lg text-white text-[20px] font-medium'>Expolore More</button>
+      </div>
+
+
+
 
     </div>
   )
